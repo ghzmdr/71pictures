e427f486024b4a6900e2cbfdc523e771ec99f047
Vincent <ghzmdr@gmail.com>
2017-09-19 23:58:16 +0200

    [+] Release task 🤘

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var path = require("path");
 var gulp = require("gulp");
 var gutil = require("gulp-util");
+var zip = require("gulp-zip");
 var webpack = require("webpack");
 var sass = require('gulp-sass');
 var sourcemaps = require('gulp-sourcemaps');
@@ -31,9 +32,46 @@ var webpackDevConfig = {
 	}
 };
 
+var webpackProdConfig = {
+	entry: path.join(__dirname, 'src/js/main.js'),
+	output: {
+		path: path.join(__dirname, "assets/js/"),
+		filename: "[name].js"
+	},
+
+	module: {
+		rules: [
+			{
+			  test: /\.js$/,
+			  exclude: /(node_modules)/,
+			  use: {
+			    loader: 'babel-loader',
+			    options: {
+			      presets: [['env', { debug: false }]]
+			    }
+			  }
+			}
+		]
+	},
+
+	plugins: [
+		new webpack.optimize.UglifyJsPlugin()
+	]
+};
+
 gulp.task("webpack:dev", function(callback) {
     
     webpack(webpackDevConfig, function(err, stats) {
+        if(err) throw new gutil.PluginError("webpack", err);
+        gutil.log("[webpack]", stats.toString());
+        callback();
+    });
+
+});
+
+gulp.task("webpack:prod", function(callback) {
+    
+    webpack(webpackProdConfig, function(err, stats) {
         if(err) throw new gutil.PluginError("webpack", err);
         gutil.log("[webpack]", stats.toString());
         callback();
@@ -63,7 +101,20 @@ gulp.task('sass:prod', function () {
 
 });
 
-gulp.task('dev', ['webpack:dev', 'sass:dev'], function () {
+gulp.task('develop', ['webpack:dev', 'sass:dev'], function () {
 	gulp.watch('./src/js/**/*.js', ['webpack:dev']);
 	gulp.watch('./src/scss/**/*.scss', ['sass:dev']);
+})
+
+gulp.task('release', ['webpack:prod', 'sass:prod'], function () {
+	
+	var date = new Date().toString();
+	var dateRegex = /^([A-z]+\s)+(.*)(\sGMT.+)$/;
+	var dateString = dateRegex.exec(date)[2];
+	var dateString = dateString.replace(/\s|:/g, '_');
+	console.log(dateString);
+
+	gulp.src(['./assets/**/*', './inc/**/*', './*.php', './style.css'])
+		.pipe(zip('71pictures_' + dateString + '.zip'))
+		.pipe(gulp.dest('./releases'))
 })
