81936a6012d29d9c84ff65800d487d5912f5d393
Vincent <ghzmdr@gmail.com>
2017-10-02 01:18:26 +0200

    [+] Facebook og data

--- header.php
+++ header.php
@@ -5,6 +5,11 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 
 <?php wp_head(); ?>
+
+<meta property="og:url"                content="<?php bloginfo('wpurl') ?>" />
+<meta property="og:title"              content="<?php bloginfo('name') ?>" />
+<meta property="og:description"        content="<?php bloginfo('description') ?>" />
+<meta property="og:image"              content="<?php the_field('sharing_data', 'option')['image'] ?>" />
 </head>
 
 <body>
