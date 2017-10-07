f7c1df821997cf8b3e102ecb77ec89bfe9754bb2
Vincent <ghzmdr@gmail.com>
2017-10-07 14:26:52 +0200

    [+] Articles page

--- inc/post-types.php
+++ inc/post-types.php
@@ -1,16 +1,50 @@
 <?php 
 
-function create_post_type() {
-  // register_post_type( 'main_project',
-  //   array(
-  //     'labels' => array(
-  //       'name' => __( 'Main Projects' ),
-  //       'singular_name' => __( 'Main Project' )
-  //     ),
-  //     'public' => true,
-  //     'has_archive' => true,
-  //     'supports' => array('title','author','thumbnail','excerpt')
-  //   )
-  // );
+function seventuonepictures_create_post_type() {
+  register_post_type( 'article',
+    array(
+      'labels' => array(
+        'name' => __( 'Articles' ),
+        'singular_name' => __( 'Article' )
+      ),
+      'public' => true,
+      'has_archive' => false,
+      'supports' => array('title','author','thumbnail','excerpt', 'revisions', 'custom-fields'),
+      'taxonomies' => array( 'article_types'),
+      'rewrite'                    => array( 'slug' => 'articles' )
+    )
+  );
 }
-add_action( 'init', 'create_post_type' );
+add_action( 'init', 'seventuonepictures_create_post_type' );
+
+// Register Custom Taxonomy
+function seventyone_custom_taxonomy() {
+
+  $labels = array(
+    'name'                       => 'Article Types',
+    'singular_name'              => 'Article Type',
+    'menu_name'                  => 'Article Types',
+    'parent_item'        => null,
+    'parent_item_column'     => null,
+    'show_in_rest'         => true,
+    'all_items'                  => 'All Article Types',
+    'new_item_name'              => 'New Article Type',
+    'add_new_item'               => 'Add New Article Type',
+    'edit_item'                  => 'Edit Article Type',
+    'update_item'                => 'Update Article Type',
+    'view_item'                  => 'View Article Type'
+  );
+
+  $args = array(
+    'labels'                     => $labels,
+    'hierarchical'               => false,
+    'public'                     => true,
+    'show_ui'                    => true,
+    'show_admin_column'          => true,
+    'show_in_nav_menus'          => true,
+    'show_tagcloud'              => true,
+  );
+  register_taxonomy( 'article_types', array( 'article' ), $args );
+
+}
+add_action( 'init', 'seventyone_custom_taxonomy', 0 );
--- page-articles.php
+++ page-articles.php
@@ -17,6 +17,22 @@
 		</h5>
 	</header>
 
+	<div class="article-selector">
+		<ul class="list list--horizontal article-selector__types">
+			<li class="list-item article-selector__type" data-article-type="all">
+				All
+			</li>
+			<?php $article_types = get_terms( array('taxonomy'=>'article_types', 'hide_empty' => false )); 	
+			foreach ($article_types as $article_type) { ?>
+				<?php $article_type = get_object_vars($article_type); ?>
+				<li class="list-item article-selector__type" data-article-type="<?php echo $article_type['slug']; ?>">
+					<?php echo $article_type['name']; ?>
+				</li>
+			<?php } ?>
+		</ul>
+	</div>
+
+
 </article>
 
 <?php get_footer();
