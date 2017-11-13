ad05a84fdd05d34820f871fb7dfc4108f5480e41
Vincent No <ghzmdr@gmail.com>
2017-11-13 20:25:03 +0100

    [+] Stuff

--- button-menu.php
+++ button-menu.php
@@ -1,4 +1,9 @@
 <button class="button button-menu js-button-menu">
+    <svg class="button-menu__border" viewBox="0 0 10 10">
+        <path class="button-menu__border-green" d="M0,10 L0,0 L10,0" stroke="green"></path>
+        <path class="button-menu__border-blue" d="M0,0 L10,0 L10,10" stroke="blue"></path>
+        <path class="button-menu__border-red" d="M10,0 L10,10 L0,10 L0,0" stroke="red"></path>
+    </svg>
     <span class="button-menu__icon button-menu-hamburger">
         <span class="button-menu-hamburger__line js-burger-line"></span>
         <span class="button-menu-hamburger__line js-burger-line"></span>
